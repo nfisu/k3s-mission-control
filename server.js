21001287bb43392bcbdf49ab38db4fbc56ae2535
Ubuntu <ubuntu@ip-172-31-20-16.eu-west-2.compute.internal>
2026-04-23 22:39:17 +0000

    feat: v5 broken rollback demo

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+throw new Error('Helm rollback demo - deliberate crash!');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;

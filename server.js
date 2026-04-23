0ac2e7e72a87332ccf3e2b1a32795f10e003ec47
Ubuntu <ubuntu@ip-172-31-20-16.eu-west-2.compute.internal>
2026-04-23 22:48:41 +0000

    fix: remove crash deploy v5 fixed

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-throw new Error('Helm rollback demo - deliberate crash!');
-
 const app = express();
 
 const PORT = process.env.PORT || 3000;
